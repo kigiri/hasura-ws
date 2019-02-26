6989b716ae2217cea3cca005efd9a78088d76caa
Clement Denis <le.mikmac@gmail.com>
2019-02-26 20:01:23 +0000

    add build files

--- packages/model/dist/index.js
+++ packages/model/dist/index.js
@@ -4,57 +4,53 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var hooks = require('@hasura-ws/hooks');
 
+const withHook = (useHook, query, name) => id => {
+  const ret = useHook(query, { id }, [id]);
+  return ret.value ? { [name]: ret.value[name][0] } : ret
+};
+
 const buildModel = prepare => (name, types) => {
   const all = `{id ${types}}`;
   const oneById = `($id: Int!) {
     ${name} (where: {id: {_eq: $id}} limit: 1) ${all}
   }`;
 
-  const select = prepare(`query ${oneById}`);
-  const subscribe = prepare(`subscription ${oneById}`);
+  const selectQuery = prepare(`query ${oneById}`);
+  const subscribeQuery = prepare(`subscription ${oneById}`);
 
-  const insert = prepare(`
+  const insertQuery = prepare(`
   mutation insert_${name} ($objects: [${name}_insert_input!]!){
     insert_${name} (objects: $objects) { returning { id } }
   }`);
 
-  const update = prepare(`
+  const updateQuery = prepare(`
   mutation update_${name}($id: Int, $changes: ${name}_set_input) {
     update_${name}(where: {id: {_eq: $id}}, _set: $changes) { affected_rows }
   }`);
 
-  const remove = prepare(`
+  const deleteQuery = prepare(`
   mutation delete_${name} {
     delete_${name} (where: {id: {_eq: $id}}) { affected_rows }
   }`);
 
-  const updateWithId = (id, changes) => update({ id, changes });
-  const updateWithoutId = ({ id, ...changes }) => update({ id, changes });
-
   return {
-    queries: { insert, select, subscribe, remove },
-    get: async id => (await select({ id }))[name][0],
+    insertQuery,
+    deleteQuery,
+    updateQuery,
+    selectQuery,
+    get: async id => (await selectQuery({ id }))[name][0],
     add: async o =>
-      (await insert({ objects: [o] }))[`insert_${name}`].returning[0].id,
-    update: (id, changes) => {
-      if (typeof id === 'object') {
-        if (!id) {
-          throw Error(`Update need an id to select which ${name} to update`)
-        }
-        return updateWithoutId(id)
-      }
-      if (!changes || typeof changes !== 'object') {
-        throw Error(`Update was called without any changes`)
-      }
-      return updateWithId(id, changes)
-    },
-    subscribe: (id, sub) => subscribe(result => sub(result[name][0]), { id }),
-    remove: id => remove({ id }),
-    useGet: id => hooks.useQuery(select, { id }, [id]),
-    useRemove: id => hooks.useMutation(remove, { id }, [id]),
-    useSubscribe: id => hooks.useSubscribe(subscribe, { id }, [id]),
-    useAdd: (vars, inputs) => hooks.useMutation(insert, vars, inputs),
-    useUpdate: (vars, inputs) => hooks.useMutation(update, vars, inputs),
+      (await insertQuery({ objects: [o] }))[`insert_${name}`].returning[0].id,
+    update: ({ id, ...changes }) => updateQuery({ id, changes }),
+    subscribe: (id, sub) =>
+      subscribeQuery(result => sub(result[name][0]), { id }),
+    remove: id => deleteQuery({ id }),
+    useGet: withHook(hooks.useQuery, selectQuery, name),
+    useSubscribe: withHook(hooks.useSubscribe, subscribeQuery, name),
+    useRemove: id => hooks.useMutation(deleteQuery, { id }, [id]),
+    useAdd: (o, inputs) => hooks.useMutation(insertQuery, { objects: [o] }, inputs),
+    useUpdate: ({ id, ...changes }, inputs) =>
+      hooks.useMutation(updateQuery, { id, changes }, inputs),
   }
 };
 
